131f5fe1c87761af49bd0f38e3084bf113c59667
kafkouf <yassineab1212@gmail.com>
2022-08-26 00:14:32 +0100

    on work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 24h-project
 the game that test your Focus lvl
+.it's a game where it desplayes the color word in a different color value and your got either color value or color word to Choose
+.you need to focus on the question so you get the right answer and get more points
+.you can choose your timmer to test your lvl of focus  
---
+++ guess.js
@@ -0,0 +1,137 @@
+$('.container').css({
+    'width':'70%',
+    'height':'500px',
+    
+    'margin-left':'275px'
+})
+$('.butdiv').css({
+    'margin-left':'580px',
+    'margin-top':'50px'
+})
+$('.d').css({
+    'height':'150px',
+    'width':'25%',
+   
+    'float':'left',
+    'margin-left':'50px',
+    'margin-top':'165px'
+
+})
+
+
+function randInt(n) { 
+    return Math.floor(Math.random() * (n + 1)) 
+    }
+
+    var questiondata=['what is the color ?','what is the word?']
+    var colordata=['red','yellow','green','blue','black','pink','purple' , 'orange' ,'aqua' ,'brown' ,'grey']
+    var answers=[$('.answer1').html(),$('.answer2').html()]
+    
+     var randqesust=function(){
+       
+           $('.question').append(questiondata[randInt(1)])
+        
+     }
+     //// work and need to delete the prev item
+    //  var randcolor=function(){
+    //    // got a proble that some times give empty 
+    //     $('.answer1').append(colordata[randInt(11)])
+    //     $('.answer2').append(colordata[randInt(11)])
+
+    //  }
+
+var score=0
+var wronganswers=0
+var counter=0
+var lvl=$('#mySelect').val()
+function theTimer(){
+    if ($('#mySelect').val()==='expert'){start(30)}
+    else if ($('#mySelect').val()==='medium'){start(45)}
+    else if ($('#mySelect').val()==='beginner'){start(60)}
+}
+function start(d){
+ let time=d
+const count=setInterval(() =>{
+    time --;
+    $('.timee').html(time);
+    if(time<0){
+  clearInterval(count)
+alert('time is over ,your score is '+score+' in the '+lvl+' level ,try again ?')}
+}, 1000);}
+
+    
+
+        var randsug=function(){
+         var colorword=colordata[randInt(11)]
+         var colorvalue=colordata[randInt(11)]
+         console.log('this is color',colorword,'this is colorvalue',colorvalue)
+         
+         
+        $('.answer2').append(colorvalue)
+        $('.answer1').append(colorword)
+        $('.neded').append(colorword)
+        $('.neded').css('color',colorvalue)
+       
+        if(($('.answer1').html()&& $('.answer2').html()&&$('.neded').html())===''){
+            colorword=colordata[randInt(11)]
+           colorvalue=colordata[randInt(11)]
+     
+        }
+        
+    }
+    //  }
+// ('.answer2').html() to compare same suggestion so they get always difiren color name
+$('.start').click(function(){
+    theTimer()
+    randsug()
+    randqesust()
+   
+   
+})
+$('.restart').click(function(){
+    theTimer()
+    randsug()
+    randqesust()
+   
+   
+})
+
+$('.answer1').click(function(){
+    console.log('this is c',counter)
+if($('.question').html()==='what is the word?'){score++
+    $('.scr').html(score)}
+    else(wronganswers++)
+    
+    $('.answer1').empty()
+    $('.answer2').empty()
+    $('.question').empty()
+    $('.neded').empty()
+    
+    randsug()
+    randqesust()
+    
+    scores.push(score)
+    
+})
+
+$('.answer2').click(function(){
+    console.log('this is c',counter)
+    if($('.question').html()==='what is the color ?'){score++
+        $('.scr').html(score)}
+    else(wronganswers++)
+   
+    $('.answer1').empty()
+    $('.answer2').empty()
+    $('.question').empty()
+    $('.neded').empty()
+    
+    randsug()
+    randqesust()
+    
+    counter++
+    if(counter==10){return score}
+})
+var scores=[];
+//we need to local storage the scores so we can find the history of the scors
+
+
